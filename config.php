1e76f11aeb892686095b7ee8a4c4c15b771bba3c
kshitij <kshitijbansod47@gmail.com>
2021-04-13 19:32:27 +0530

    config file update

--- config.php
+++ config.php
@@ -12,14 +12,23 @@
 
     $servername="remotemysql.com";
 	$username="hXKzQw6QRS";
-	$password=" YE2uVVDdzc";
+	$password="YE2uVVDdzc";
     $dbname="nKOwEQAS";
-				error_reporting(E_ERROR | E_PARSE);
+				// error_reporting(E_ERROR | E_PARSE);
 		
-	$conn= new mysqli($servername,$username,$password,$dbname);
+	// $conn= new mysql($servername,$username,$password,$dbname);
+$conn = "mysql:host=$servername;dbname=$dbname;charset=$charset";
 
+try{
+	$pdo = new PDO($conn,$servername,$pass);
+	$pdo->setAttribute(PDO:ATTR_ERRMODE,PDO:ERRMODE_EXCEPTION);
+}
+	catch(PDOExpection $e){
+		throw new PDOExpection($e->getMessage());
+	}
+}
 
-
+$crud = new crud($pdo);
 
 
 ?>
