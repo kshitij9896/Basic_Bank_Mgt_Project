bd7d0a370d0a0a4ae4727a90fa2274641190cbf3
kshitij <kshitijbansod47@gmail.com>
2021-04-13 18:53:49 +0530

    remote connection added

--- config.php
+++ config.php
@@ -1,12 +1,23 @@
 <?php
+//on local machine
+ //    $servername="localhost";
+	// $username="root";
+	// $password="";
+ //    $dbname="bank";
+	// 			error_reporting(E_ERROR | E_PARSE);
+		
+	// $conn= new mysqli($servername,$username,$password,$dbname);
 
-    $servername="localhost";
-	$username="root";
-	$password="";
-    $dbname="bank";
+// remote database connection
+
+    $servername="remotemysql.com";
+	$username="hXKzQw6QRS";
+	$password=" YE2uVVDdzc";
+    $dbname="nKOwEQAS";
 				error_reporting(E_ERROR | E_PARSE);
 		
 	$conn= new mysqli($servername,$username,$password,$dbname);
+
 
 
 
